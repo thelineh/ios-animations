be0349c7b233bc15129ff0949e21611114aaf152
thelineh <almeida.aline2001@gmail.com>
2021-02-10 10:15:09 -0300

    create xcode proj

---
+++ ios-animations/ios-animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-animations
+//
+//  Created by Aline Almeida on 10/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
